a7c866f26d0bdb8521ee669a58f849fe7b431748
gohh56 <ystokio@gmail.com>
2018-12-09 01:23:38 +0900

    add unit test cases

--- server/utils.test.js
+++ server/utils.test.js
@@ -10,4 +10,16 @@ describe('parsePathParameters', () => {
   //
   // TODO: parsePathParameters のユニットテストを追加しよう
   //
+  test('should return correct parameters', () => {
+    const pathParameters = parsePathParameters('/posts/3/comments/4', '/posts/:postId/comments/:commentId');
+    expect(pathParameters).toBeDefined();
+    expect(pathParameters.postId).toBe('3');
+    expect(pathParameters.commentId).toBe('4');
+  });
+
+  test('should return correct parameters', () => {
+    const pathParameters = parsePathParameters('/test', '/:testId');
+    expect(pathParameters).toBeDefined();
+    expect(pathParameters.testId).toBe('test');
+  });
 });
